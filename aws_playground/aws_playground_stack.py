2b6e1fb58aa205a107452aac1761b0d84884c60c
cyoung43 <cyoung43@byu.edu>
2023-02-17 15:24:29 -0700

    have boto3 and lambda function working

--- aws_playground/aws_playground_stack.py
+++ aws_playground/aws_playground_stack.py
@@ -4,6 +4,8 @@ from aws_cdk import (
     aws_s3 as _s3,
     aws_lambda as _lambda,
     aws_s3_notifications as s3n,
+    RemovalPolicy,
+    aws_iam as iam,
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -21,10 +23,13 @@ class AwsPlaygroundStack(Stack):
         #     visibility_timeout=Duration.seconds(300),
         # )
 
-        s3 = _s3.Bucket(self, 'testing-bucket')
+        s3 = _s3.Bucket(self, 'testing-bucket', removal_policy=RemovalPolicy.DESTROY, auto_delete_objects=True)
+        # s3.apply_removal_policy(RemovalPolicy.DESTROY)
 
-        lamb = _lambda.Function(self, 'testing-lambda', runtime=_lambda.Runtime.PYTHON_3_7, handler='lambda_handler.main', code=_lambda.Code.from_asset('./lambda'))
+        lamb = _lambda.Function(self, 'testing-lambda', runtime=_lambda.Runtime.PYTHON_3_7, handler='lambda_handler.main', code=_lambda.Code.from_asset('aws_playground/lambda'))
+        lamb.add_to_role_policy(iam.PolicyStatement(actions=['s3:*'], resources=['*']))
 
         notification = s3n.LambdaDestination(lamb)
 
         s3.add_event_notification(_s3.EventType.OBJECT_CREATED, notification)
+        s3.grant_read_write(lamb)
--- aws_playground/lambda/lambda_handler.py
+++ aws_playground/lambda/lambda_handler.py
@@ -1,9 +1,27 @@
 import json
 import boto3
 
-def handler(event, context):
+BUCKET = 'awsplaygroundstack-testingbucket7061cae3-hdvce056y7xq'
+
+def get_bucket():
+    s3 = boto3.resource('s3')
+
+    bucket = s3.Bucket(BUCKET)
+
+    return bucket
+
+def main(event, context):
 
     print(event)
+
+    bucket = get_bucket()
+
+    if bucket:
+        print('bucket found!')
+        # list all objects in the bucket
+        for obj in bucket.objects.all():
+            metadata = obj.get()
+            print(metadata)
 
     return {
         'statusCode': 200,
